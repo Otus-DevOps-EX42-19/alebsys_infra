14a772203c6a48a20c030e5e84c4b5bceff1a4ab
AleksLebedev <root@debian>
2019-08-29 11:23:34 -0400

    learn packer

--- gcloud.sh
+++
@@ -1,8 +0,0 @@
-gcloud compute instances create reddit-app_test \
---boot-disk-size=10GB \
---image-family ubuntu-1604-lts \
---image-project=ubuntu-os-cloud \
---machine-type=g1-small \
---tags puma-server \
---restart-on-failure\
---metadata-from-file startup-script=/home/aleb/startup_script.sh
